798e712e62b0f54fcb359edb6bdb4fe95cfb4e2d
Adrianna <dziewulska37@gmail.com>
2025-03-17 09:50:54 +0100

    LAB3

---
+++ LAB3 20250310/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private int userId;
+    private String firstName;
+    private String lastName;
+    public User(int userId, String firstName, String lastName){
+        this.userId=userId;
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+}
